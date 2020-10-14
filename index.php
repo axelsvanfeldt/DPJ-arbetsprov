a17a66a8a0ed67701ee6807fb3a4e36ff061f0d0
Axel <axel.svanfeldt@gmail.com>
2020-10-14 09:40:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<title>DPJ Arbetsprov</title>
+	<meta name="author" content="">
+	<meta name="description" content="Ett arbetsprov av DPJ">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
+    <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</head>
+
+<body>
+	<main>
+        
+        <h1>dpj arbetsprov</h1>
+        
+        <form>
+            <input type="search" placeholder="ange sökterm">
+        </form>
+        
+        <div id="result">
+            <span>inget resultat</span>
+        </div>
+        
+        <img class="logo" src="img/logo.png" alt="logo">
+        
+    </main>
+</body>
+
+</html>
