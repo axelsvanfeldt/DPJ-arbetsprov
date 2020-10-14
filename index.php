bcf4a2ec7a125197b52fd85e68ede28ae492a5e9
Axel <axel.svanfeldt@gmail.com>
2020-10-14 10:35:03 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -2,24 +2,23 @@
 <html lang="">
 
 <head>
-	<meta charset="utf-8">
-	<title>DPJ Arbetsprov</title>
-	<meta name="author" content="">
-	<meta name="description" content="Ett arbetsprov av DPJ">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>DPJ Arbetsprov</title>
+    <meta name="author" content="">
+    <meta name="description" content="Ett arbetsprov av DPJ">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
     <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="js/app.js"></script>
 </head>
 
 <body>
-	<main>
-        
+   <main>
         <h1>dpj arbetsprov</h1>
-        
-        <form>
+    
+       	<form>
             <input type="search" placeholder="ange sökterm">
         </form>
         
@@ -28,7 +27,6 @@
         </div>
         
         <img class="logo" src="img/logo.png" alt="logo">
-        
     </main>
 </body>
 
